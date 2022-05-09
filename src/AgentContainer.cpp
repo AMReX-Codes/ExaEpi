8d0d5eb8a006a4e462f75b91e6f2846ea2e8d075
Andrew Myers <atmyers2@gmail.com>
2022-05-09 14:23:39 -0700

    realistic initial condition code

--- src/AgentContainer.cpp
+++ src/AgentContainer.cpp
@@ -6,40 +6,105 @@ void AgentContainer::initAgents ()
 {
     BL_PROFILE("AgentContainer::initAgents");
 
-    // This gives us 1000 particles with random positions and unique ids,
-    // all other components are zeroed out.
-    Long num_agents = 1000;
-    ULong iseed = 451;
-    bool serialize = false;
+    int ncell = 3000;
 
-    ParticleInitData pdata;
-    pdata.real_array_data.fill(0.0);
-    pdata.int_array_data.fill(0);
-    this->InitRandom(num_agents, iseed, pdata, serialize);
+    int num_pop_bins = 1000;
+    amrex::Real log_min_pop = 1.062;
+    amrex::Real log_max_pop = 4.0;
+    amrex::Vector<amrex::Real> cell_pop_bins_r(num_pop_bins);
+    amrex::Vector<amrex::Real> num_cells_per_bin_r(num_pop_bins);
 
-    // Now we go back and make some of them positive, currently
-    // no other attributes are actually used.
-    for (int lev = 0; lev <= finestLevel(); ++lev)
-    {
-        auto& plev  = GetParticles(lev);
-        for(MFIter mfi = MakeMFIter(lev); mfi.isValid(); ++mfi)
-        {
-            int gid = mfi.index();
-            int tid = mfi.LocalTileIndex();
-            auto& ptile = plev[std::make_pair(gid, tid)];
-            auto& soa   = ptile.GetStructOfArrays();
-            const size_t np = soa.numParticles();
-            auto status_ptr = soa.GetIntData(IntIdx::status).data();
+    for (int i = 0; i < cell_pop_bins_r.size(); ++i) {
+        cell_pop_bins_r[i] = std::pow(10.0,
+            log_min_pop + i*(log_max_pop - log_min_pop)/(num_pop_bins-1));
+        num_cells_per_bin_r[i] = std::pow(cell_pop_bins_r[i], -1.5);
+    }
 
-            amrex::ParallelForRNG( np,
-            [=] AMREX_GPU_DEVICE (int i, RandomEngine const& engine) noexcept
-            {
-                if (amrex::Random(engine) < 0.05) {
-                    status_ptr[i] = 1;
-                }
-            });
+    amrex::Real norm = 0;
+    for (int i = 0; i < num_cells_per_bin_r.size(); ++i) {
+        norm += num_cells_per_bin_r[i];
+    }
+
+    amrex::Vector<int> cell_pop_bins(num_pop_bins);
+    amrex::Vector<int> num_cells_per_bin(num_pop_bins);
+    for (int i = 0; i < num_cells_per_bin.size(); ++i) {
+        num_cells_per_bin_r[i] *= (ncell*ncell/norm);
+        num_cells_per_bin[i] = std::round(num_cells_per_bin_r[i]);
+        cell_pop_bins[i] = std::round(cell_pop_bins_r[i]);
+    }
+
+    int total_cells = 0;
+    for (int i = 0; i < num_cells_per_bin.size(); ++i) {
+        total_cells += num_cells_per_bin[i];
+    }
+    num_cells_per_bin[0] += (ncell*ncell - total_cells);
+
+    int total_agents = 0;
+    for (int i = 0; i < num_cells_per_bin.size(); ++i) {
+        total_agents += cell_pop_bins[i]*num_cells_per_bin[i];
+    }
+    amrex::Print() << "Total number of agents: " << total_agents << "\n";
+
+    std::vector<int> perm(ncell*ncell);
+    std::iota(perm.begin(), perm.end(), 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(perm.begin(), perm.end(), g);
+
+    Vector<int> offsets(num_pop_bins+1);
+    offsets[0] = 0;
+    for (int i = 1; i < num_pop_bins+1; ++i) {
+        offsets[i] = offsets[i-1] + num_cells_per_bin[i-1];
+    }
+
+    Vector<int> cell_pops(ncell*ncell, -1);
+    for (int i = 0; i < num_pop_bins; ++i) {
+        for (int j = offsets[i]; j < offsets[i+1]; ++j) {
+            cell_pops[perm[j]] = cell_pop_bins[i];
         }
     }
+
+    amrex::Gpu::DeviceVector<int> cell_pops_d(cell_pops.size());
+    amrex::Gpu::DeviceVector<int> cell_offsets_d(cell_pops.size()+1);
+    Gpu::copy(Gpu::hostToDevice, cell_pops.begin(), cell_pops.end(), cell_pops_d.begin());
+    Gpu::exclusive_scan(cell_pops_d.begin(), cell_pops_d.end(), cell_offsets_d.begin());
+
+    // Fill in particle data in each cell
+    auto& ptile = DefineAndReturnParticleTile(0, 0, 0);
+    ptile.resize(total_agents);
+
+    auto& soa   = ptile.GetStructOfArrays();
+    auto& aos   = ptile.GetArrayOfStructs();
+    auto pstruct_ptr = aos().data();
+    auto status_ptr = soa.GetIntData(IntIdx::status).data();
+    auto timer_ptr = soa.GetIntData(RealIdx::timer).data();
+
+    auto cell_offsets_ptr = cell_offsets_d.data();
+
+    amrex::ParallelForRNG( total_agents,
+    [=] AMREX_GPU_DEVICE (int i, RandomEngine const& engine) noexcept
+    {
+        // find out which cell particle i needs to go in.
+        int cell_id = 0;
+        while (cell_offsets_ptr[cell_id] < i) {++cell_id;}
+
+        int idx = cell_id % ncell;
+        int idy = cell_id / ncell;
+
+        auto& p = pstruct_ptr[i];
+        p.pos(0) = idx + 0.5;
+        p.pos(1) = idy + 0.5;
+        p.id() = i;
+        p.cpu() = 0;
+
+        timer_ptr[i] = 0.0;
+
+        if (amrex::Random(engine) < 0.05) {
+            status_ptr[i] = 1;
+        }
+    });
+
+    Redistribute();
 }
 
 void AgentContainer::moveAgents ()
@@ -81,10 +146,7 @@ void AgentContainer::interactAgents ()
 {
     BL_PROFILE("AgentContainer::interactAgents");
 
-    // the dx used to bin the particles for spread is currently
-    // twice the dx used to move the particles...
-
-    IntVect bin_size = {AMREX_D_DECL(2, 2, 2)};
+    IntVect bin_size = {AMREX_D_DECL(1, 1, 1)};
     for (int lev = 0; lev < numLevels(); ++lev)
     {
         const Geometry& geom = Geom(lev);
